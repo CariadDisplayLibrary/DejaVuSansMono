0624d7e43266d905525e97514b1c889e570ea4cb
Matt Jenkins <matt@majenko.co.uk>
2020-05-02 19:54:30 +0100

    Renamed DisplayCore to Cariad

--- src/DejaVuSansMono.h
+++ src/DejaVuSansMono.h
@@ -1,7 +1,7 @@
 #ifndef _FONTS_DEJAVUSANSMONO_H
 #define _FONTS_DEJAVUSANSMONO_H
 
-#include <DisplayCore.h>
+#include <Cariad.h>
 
 namespace Fonts {
         extern const uint8_t DejaVuSansMono8[];
